6eb9be0a571c045cea971d75c106a2be42dde931
Youngro Kim <rich@imac>
2016-02-28 16:11:30 -0800

    Initial Commit

---
+++ PointLine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PointLine
+//
+//  Created by Rich Kim on 2/28/16.
+//  Copyright © 2016 Rich Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
